7d656daf866d8b756dbdfd66e9fa9611057ad540
Gustavo <gustavo.zucolo@hotmail.com>
2023-01-30 21:17:12 -0300

    ItemComicCard.kt created

---
+++ app/src/main/java/com/example/gmzucolo/marvel_app/ui/component/ItemComicCard.kt
@@ -0,0 +1,58 @@
+package com.example.gmzucolo.marvel_app.ui.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
+import androidx.compose.ui.unit.dp
+import com.example.gmzucolo.marvel_app.R
+import com.example.gmzucolo.marvel_app.ui.theme.MarvelappTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ItemComicCard() {
+    Card(modifier = Modifier
+        .fillMaxWidth()
+        .padding(8.dp)
+        .clickable { }) {
+        Column(modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .clickable { }) {
+            Image(
+                painter = painterResource(id = R.drawable.marvel_logo_large),
+                contentDescription = ""
+            )
+            Text(text = "Name", modifier = Modifier.padding(vertical = 8.dp))
+            Text(
+                text = LoremIpsum(100).toString(),
+                modifier = Modifier.padding(vertical = 8.dp)
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ItemComicCardLightPreview() {
+    MarvelappTheme(darkTheme = false) {
+        ItemComicCard()
+    }
+}
+
+@Preview
+@Composable
+fun ItemComicCardDarkPreview() {
+    MarvelappTheme(darkTheme = true) {
+        ItemComicCard()
+    }
+}

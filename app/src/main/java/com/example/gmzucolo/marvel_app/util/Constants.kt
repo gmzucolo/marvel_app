fffdeb20ca006034f26665514e8846b61a8150e0
Gustavo <gustavo.zucolo@hotmail.com>
2023-02-23 22:08:35 -0300

    Module.kt config

--- app/src/main/java/com/example/gmzucolo/marvel_app/util/Constants.kt
+++ app/src/main/java/com/example/gmzucolo/marvel_app/util/Constants.kt
@@ -4,4 +4,9 @@ object Constants {
     const val BASE_URL = "https://gateway.marvel.com/V1/public/"
     const val PUBLIC_KEY = "1c9a9078aa51928c6a1fc35082af9113"
     const val PRIVATE_KEY = "90e2d0589a6b6675159cf2ddb031ea711968f0fc"
+
+    const val APIKEY = "apikey"
+    const val HASH = "hash"
+    const val TS = "ts"
+
 }

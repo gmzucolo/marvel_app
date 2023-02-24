2dc54c4ae87675d77d51c85e9bf3ceefd343aea8
Gustavo <gustavo.zucolo@hotmail.com>
2023-02-23 21:24:38 -0300

    ServiceApi.kt config

---
+++ app/src/main/java/com/example/gmzucolo/marvel_app/util/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.gmzucolo.marvel_app.util
+
+object Constants {
+    const val BASE_URL = "https://gateway.marvel.com/V1/public/"
+    const val PUBLIC_KEY = "1c9a9078aa51928c6a1fc35082af9113"
+    const val PRIVATE_KEY = "90e2d0589a6b6675159cf2ddb031ea711968f0fc"
+}

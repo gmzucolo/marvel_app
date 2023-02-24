c2da0a743b1ba4e3378e19445a9040f8a27ecbd8
Gustavo <gustavo.zucolo@hotmail.com>
2023-02-23 21:42:30 -0300

    Module.kt config fix

--- app/src/main/java/com/example/gmzucolo/marvel_app/di/Module.kt
+++ app/src/main/java/com/example/gmzucolo/marvel_app/di/Module.kt
@@ -5,13 +5,14 @@ import com.example.gmzucolo.marvel_app.util.Constants.BASE_URL
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
-@InstallIn
+@InstallIn(SingletonComponent::class)
 object Module {
 
     @Singleton

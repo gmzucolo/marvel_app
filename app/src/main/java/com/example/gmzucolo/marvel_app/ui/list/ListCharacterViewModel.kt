8b508f1759d17e883b4f00096c366fbd87c8e234
Gustavo <gustavo.zucolo@hotmail.com>
2023-02-26 12:29:47 -0300

    ListCharacterViewModel.kt created

--- app/src/main/java/com/example/gmzucolo/marvel_app/ui/list/ListCharacterViewModel.kt
+++ app/src/main/java/com/example/gmzucolo/marvel_app/ui/list/ListCharacterViewModel.kt
@@ -1,6 +1,55 @@
 package com.example.gmzucolo.marvel_app.ui.list
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.gmzucolo.marvel_app.data.model.character.CharacterModelResponse
+import com.example.gmzucolo.marvel_app.repository.MarvelRepository
+import com.example.gmzucolo.marvel_app.util.ResourceState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import retrofit2.Response
+import java.io.IOException
+import javax.inject.Inject
 
-class ListCharacterViewModel : ViewModel() {
+@HiltViewModel
+class ListCharacterViewModel @Inject constructor(
+    private val repository: MarvelRepository
+) : ViewModel() {
+
+    private val _list =
+        MutableStateFlow<ResourceState<CharacterModelResponse>>(ResourceState.Loading())
+    val list: StateFlow<ResourceState<CharacterModelResponse>> = _list
+
+    init {
+        fetch()
+    }
+
+    private fun fetch() = viewModelScope.launch {
+        safeFetch()
+    }
+
+    private suspend fun safeFetch() {
+        try {
+            val response = repository.list()
+            _list.value = handleResponse(response)
+        } catch (t: Throwable) {
+            when (t) {
+                is IOException -> _list.value =
+                    ResourceState.Error("Erro de conexão com a internet")
+                else -> _list.value = ResourceState.Error("Falha na conversão de dados")
+            }
+        }
+    }
+
+    private fun handleResponse(response: Response<CharacterModelResponse>): ResourceState<CharacterModelResponse> {
+        if (response.isSuccessful) {
+            response.body()?.let { values ->
+                return ResourceState.Success(values)
+            }
+        }
+        return ResourceState.Error(response.message())
+    }
+
 }

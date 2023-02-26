81df90d7b725faa1749ae8e1df6d32dacfdb4277
Gustavo <gustavo.zucolo@hotmail.com>
2023-02-26 12:03:26 -0300

    MarvelRepository.kt created

---
+++ app/src/main/java/com/example/gmzucolo/marvel_app/repository/MarvelRepository.kt
@@ -0,0 +1,11 @@
+package com.example.gmzucolo.marvel_app.repository
+
+import com.example.gmzucolo.marvel_app.data.remote.ServiceApi
+import javax.inject.Inject
+
+class MarvelRepository @Inject constructor(
+    private val api: ServiceApi
+) {
+    suspend fun list(nameStartsWith: String) = api.list(nameStartsWith)
+    suspend fun getComics(characterId: Int) = api.getComics(characterId)
+}

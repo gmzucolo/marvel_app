6dc421907e20e653e842b443a49ddcd89c8c224e
Gustavo <gustavo.zucolo@hotmail.com>
2023-04-11 19:57:44 -0300

    titulo e icones trocados

--- app/src/main/java/com/example/gmzucolo/marvel_app/MainActivity.kt
+++ app/src/main/java/com/example/gmzucolo/marvel_app/MainActivity.kt
@@ -3,12 +3,14 @@ package com.example.gmzucolo.marvel_app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.NavHost
@@ -94,7 +96,12 @@ fun MarvelApp(
     Scaffold(
         topBar = {
             CenterAlignedTopAppBar(
-                title = { Text(text = "Marvel Characters") },
+                title = {
+                    Image(
+                        painter = painterResource(id = R.drawable.marvel_logo_large),
+                        contentDescription = "logo marvel"
+                    )
+                },
             )
         },
         bottomBar = {

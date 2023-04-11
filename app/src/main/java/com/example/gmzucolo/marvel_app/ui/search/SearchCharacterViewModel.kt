bd3d45d9575ad34a5ebf90e1082fc59c42d5a6fc
Gustavo <gustavo.zucolo@hotmail.com>
2023-04-11 20:36:12 -0300

    SearchCharacterViewModel.kt created

--- app/src/main/java/com/example/gmzucolo/marvel_app/ui/search/SearchCharacterViewModel.kt
+++ app/src/main/java/com/example/gmzucolo/marvel_app/ui/search/SearchCharacterViewModel.kt
@@ -1,6 +1,53 @@
 package com.example.gmzucolo.marvel_app.ui.search
 
 import androidx.lifecycle.ViewModel
+import com.example.gmzucolo.marvel_app.data.model.character.CharacterModelResponse
+import com.example.gmzucolo.marvel_app.repository.MarvelRepository
+import com.example.gmzucolo.marvel_app.util.ResourceState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import retrofit2.Response
+import java.io.IOException
+import javax.inject.Inject
 
-class SearchCharacterViewModel : ViewModel() {
+@HiltViewModel
+class SearchCharacterViewModel @Inject constructor(
+    private val repository: MarvelRepository
+) : ViewModel() {
+
+    private val _searchCharacter =
+        MutableStateFlow<ResourceState<CharacterModelResponse>>(ResourceState.Empty())
+    val searchCharacter: StateFlow<ResourceState<CharacterModelResponse>> = _searchCharacter
+
+    private suspend fun searchCharacter(nameStartsWith: String?) {
+        _searchCharacter.value = ResourceState.Loading()
+        try {
+            val response = repository.listByStartsWith(nameStartsWith)
+            _searchCharacter.value = handleResponse(response)
+        } catch (t: Throwable) {
+            when (t) {
+                is IOException -> {
+                    _searchCharacter.value = ResourceState.Error("No internet connection")
+                }
+                else -> {
+                    _searchCharacter.value = t.message.let { ResourceState.Error(it!!) }
+                }
+            }
+        }
+
+    }
+
+    private fun handleResponse(
+        response: Response<CharacterModelResponse>
+    ): ResourceState<CharacterModelResponse> {
+
+        if (response.isSuccessful) {
+            response.body()?.let { resultResponse ->
+                return ResourceState.Success(resultResponse)
+            }
+        }
+        return ResourceState.Error(response.message())
+    }
+
 }
